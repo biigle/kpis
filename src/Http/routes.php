a855959887b21f1caa9080fb3b1aaffedf34399a
Leane Schlundt <lhecht@techfak.de>
2024-03-14 10:20:52 +0100

    Add route for actions/visits

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,3 +5,15 @@
 //    'as'   => 'quotes',
 //    'uses' => 'QuotesController@index',
 // ]);
+
+
+
+$router->group([
+    // 'namespace' => 'Api',
+    'prefix' => 'api/v1/kpis',
+    // 'middleware' => ['api', 'auth:web,api'],
+], function ($router) {
+    $router->post('actions', [
+        'uses' => 'RequestController@store'
+    ]);
+});
