b7850a5b3d17bb6e3f707ef3fae4078f6d66d374
Leane Schlundt <lhecht@techfak.de>
2024-03-21 10:28:18 +0100

    Change route path

--- src/Http/api.php
+++ src/Http/api.php
@@ -1,18 +1,5 @@
 <?php
 
-// $router->get('quotes', [
-//    'middleware' => 'auth',
-//    'as'   => 'quotes',
-//    'uses' => 'QuotesController@index',
-// ]);
-
-
-
-$router->group([
-    'prefix' => 'api/v1/kpis',
-    'middleware' => ['auth.kpis'],
-], function ($router) {
-    $router->post('actions', [
-        'uses' => 'RequestController@store'
-    ]);
-});
+$router->post('api/v1/kpis', [
+    'uses' => 'RequestController@store'
+])->middleware('auth.kpis');
