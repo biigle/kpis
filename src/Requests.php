3605284ee280d785034813f92794ef83daf55216
Leane Schlundt <lhecht@techfak.de>
2024-03-21 08:50:55 +0100

    Add get methods

--- src/Requests.php
+++ src/Requests.php
@@ -15,4 +15,19 @@ class Requests
             DB::table('kpis_visits')->insert(['date' => $today, 'value' => $visits]);
         });
     }
+
+    public static function getActions($year, $month)
+    {
+        $date = Carbon::createFromDate($year, $month, 1)->endOfMonth()->toDateString();
+        $res =  count(DB::table('kpis_actions')->where('date', '=', $date)->pluck('value')) != 0 ?
+        DB::table('kpis_actions')->where('date', '=', $date)->pluck('value')[0] : 0;
+        return $res;
+    }
+    public static function getVisits($year, $month)
+    {
+        $date = Carbon::createFromDate($year, $month, 1)->endOfMonth()->toDateString();
+        $res =  count(DB::table('kpis_visits')->where('date', '=', $date)->pluck('value')) != 0 ?
+        DB::table('kpis_visits')->where('date', '=', $date)->pluck('value')[0] : 0;
+        return $res;
+    }
 }
