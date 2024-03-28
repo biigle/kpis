08c14466c5bf972be333f6da0cd93784fb5ca71e
Leane Schlundt <lhecht@techfak.de>
2024-03-28 10:29:12 +0100

    Add comment

--- src/config/kpis.php
+++ src/config/kpis.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    'token' => env('KPI_TOKEN'),
+    /**
+     *
+     * Token to verify cron job request to save visits/actions
+     *
+     **/
+    'token' => env('KPIS_TOKEN'),
 ];
