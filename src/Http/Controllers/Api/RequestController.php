cc9849b5ae8dd74932a82609aeaba01d536bf0c0
Martin Zurowietz <martin@zurowietz.de>
2024-04-03 14:35:14 +0200

    Fix apidoc definition

--- src/Http/Controllers/Api/RequestController.php
+++ src/Http/Controllers/Api/RequestController.php
@@ -9,13 +9,15 @@ use Biigle\Http\Controllers\Controller;
 class RequestController extends Controller
 {
     /**
+     * @apiDefine kpiToken KPI Token
+     * The request must provide the token configured in the KPI module.
+     */
+
+    /**
      * Saves action/visit count from cron job
      * 
-     * @apiDefine kpi-tpken KPI Token
-     * The request must provide the token configured in the KPI module.
-     *
      * @api {post} /kpis Save actions/visits KPIs
-     * @apiPermission kpi-token
+     * @apiPermission kpiToken
      * @apiDescription The submitted values are stored for the previous day.
      * @apiParam {Number} visits The number of GET requests to `/` (without bots).
      * @apiParam {Number} actions The number of PUT/POST/DELETE requests (without bots and heartbeat).
